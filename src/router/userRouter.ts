ce15b1f86df1e9e13f427a8a02afac482cdcc44a
Matheus Augusto Moura de Lima <61365431+Mouramattheus@users.noreply.github.com>
2022-07-14 16:12:31 -0300

    cadastro-login

--- src/router/userRouter.ts
+++ src/router/userRouter.ts
@@ -1,3 +1,11 @@
 import { Router } from "express";
+import { UserController } from "../controller/UserController";
+import { UserBussines } from "../userBussines";
 
 export const userRouter = Router()
+
+const userBussines = new UserBussines()
+const userController = new UserController(userBussines)
+
+userRouter.post("/signUp",userController.signUp)
+userRouter.post("/login",userController.login)
